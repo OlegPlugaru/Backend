7afa6fd8a4be514e078166b8b11a2fabf64a01b2
OlegPlugaru <oleg.plugaru.md@gmail.com>
2023-02-17 21:41:01 +0200

    writed an even number checker

---
+++ even_num_checker.py
@@ -0,0 +1,6 @@
+value = int(input('Input a number: '))
+
+if value % 2 == 0:
+    print(f"{value} is an even number")
+else:
+    print(f"{value} is an odd number")
--- hello.py
+++ hello.py
@@ -1,6 +1,33 @@
-def greeting_function(name, age):
-    print('Welcome '+name+".You are " +str(age)+ ' years old.')
+# On example of function
+moms = [
+    ("Ned", "Eleanor"),
+    ("Max", "Susan"),
+    ("Susan", "Shelly"),
+]
 
-name = input('Enter your name: ')
-age = input('Enter you age: ')
-greeting_function(name, age)
+def find_mom(moms,child):
+    """Find the mom of child."""
+    for child_name, mom_name in moms:
+        if child == child_name:
+            return mom_name
+    return None
+
+print(find_mom(moms, "Max"))
+
+
+
+
+
+# def how_many_grandmothers(moms):
+#     """How many moms are grand - moms?"""
+#     grandmothers = 0
+#     for child, mom in moms:
+#         print(child, mom)
+#         grandma = find_mom(moms, mom)
+#         print(grandma) 
+#         if grandma:
+#             grandmothers += 1
+#     return grandmothers
+        
+
+# print(how_many_grandmothers(moms))
